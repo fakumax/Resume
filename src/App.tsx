df0ba0870cda57fcebf84c62af65879d324c83e3
Facundo Vergara <fakumax@gmail.com>
2021-08-13 23:10:48 -0300

    Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
-import * as React from "react"
+import React from "react";
 import {
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
   Grid,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import Navbar from './components/Navbar/Navbar';
 
 export const App = () => (
  
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          
-          <Link
-            color="primary"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            test 
-          </Link>
-        </VStack>
+ 
+        <Navbar />
       </Grid>
     </Box>
 

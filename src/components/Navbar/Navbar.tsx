c6bf174f05d283b5fa7918bf03cf01b15ee0d82d
Facundo Vergara <fakumax@gmail.com>
2021-08-15 00:50:13 -0300

    Changes buttons download

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -84,40 +84,43 @@ export default function WithSubnavigation() {
             CV/Resume:
           </Flex>
 
-          <Button
+          <Link
             display={{ base: 'none', md: 'inline-flex' }}
             fontSize={'sm'}
             fontWeight={700}
             color={'#232020'}
             bg={'#fff'}
+            p='10px'
             borderRadius="50%"
-            p='0'
             textTransform='uppercase'
-            href={'#'}
+            alignSelf= 'center'
+            href={'https://drive.google.com/uc?export=download&id=19f-2CacDvsE3mBeev_zl0WSFrTJuKwBr'}
             _hover={{
               bg: 'grey',
               color:'#fff'
             }}
           >
             Eng
-          </Button>
-          <Button
+          </Link>
+          
+          <Link
             display={{ base: 'none', md: 'inline-flex' }}
             fontSize={'sm'}
             fontWeight={700}
             color={'#232020'}
             bg={'#fff'}
-            href={'#'}
+            p='10px'
+            href='https://drive.google.com/uc?export=download&id=1hEg3uAEKoTSEZpbRpe3lO3yGMxPHGXEf'
             textTransform='uppercase'
             borderRadius="50%"
-            p='0'
+            alignSelf= 'center'
             _hover={{
               bg: 'grey',
               color:'#fff'
             }}
           >
             Esp
-          </Button>
+          </Link>
           <ColorModeSwitcher justifySelf="flex-end" />
         </Stack>
       </Flex>

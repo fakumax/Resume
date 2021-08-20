c37cbd0cc1c10d0dcc3d640dd1ea7f6383c1298d
Facundo Vergara <fakumax@gmail.com>
2021-08-20 19:38:21 -0300

    Body create

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Box} from "@chakra-ui/react";
 import Navbar from './components/Navbar/Navbar';
+import Body from './components/Body';
 
 export const App = () => (
  
     <Box textAlign="center" fontSize="xl">
          <Navbar />
+         <Body />
     </Box>
 
 )

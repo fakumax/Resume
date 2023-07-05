4025f102077f76de9a7ee47d26da6717c1b4ed53
Facundo Vergara <fakumax@gmail.com>
2023-07-05 14:29:31 -0300

    Upgrade setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
-import { ColorModeScript, ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 import theme from './styles/theme';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement as HTMLElement).render(
   <ChakraProvider theme={theme}>
     <ColorModeScript />
     <App />

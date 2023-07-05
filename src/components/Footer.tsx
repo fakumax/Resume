3fd26b6dc5938c59e18bfa4caabbb6b1e37f741e
Facundo Vergara <fakumax@gmail.com>
2023-07-05 17:16:30 -0300

    Fix Grid style

---
+++ src/components/Footer.tsx
@@ -0,0 +1,50 @@
+import { Flex, GridItem, HStack, Link, useColorModeValue } from '@chakra-ui/react';
+
+const Footer = () => {
+  return (
+    <GridItem colSpan={4} pb={2}>
+      <HStack justify='center'>
+        <Flex
+          textTransform='uppercase'
+          alignSelf={'center'}
+          textAlign={{ base: 'center' }}
+          fontFamily={'sans-serif'}
+          fontSize='small'
+          color={useColorModeValue('gray.800', 'white')}
+        >
+          Descargar CV/Resume:
+        </Flex>
+
+        {/* <Link
+            fontSize={'sm'}
+            fontWeight={700}
+            textTransform='uppercase'
+            alignSelf='center'
+            href={
+              'https://drive.google.com/uc?export=download&id=REDACTED_CV_ID'
+            }
+            _hover={{
+              color: '#ffc214',
+            }}
+          >
+            Eng
+          </Link> */}
+
+        <Link
+          fontSize={'sm'}
+          fontWeight={700}
+          href='https://drive.google.com/uc?export=download&id=REDACTED_CV_ID'
+          textTransform='uppercase'
+          alignSelf='center'
+          _hover={{
+            color: '#ffc214',
+          }}
+        >
+          Español
+        </Link>
+      </HStack>
+    </GridItem>
+  );
+};
+
+export default Footer;
